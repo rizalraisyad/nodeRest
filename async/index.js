178b86f4d399bacc7cd96cabc32283ecb82d0d59
rizalraisyad <muharamrizal2@gmail.com>
2021-04-04 01:09:15 +0700

    fully promise without callback

--- async/index.js
+++ async/index.js
@@ -12,27 +12,33 @@ const readFilePro = (file) => {
   });
 };
 
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
+    });
+  });
+};
+
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
 
-    superagent
-      .get(`https://dog.ceo/api/breed/${data}/images/random`)
-      .then((res) => {
-        console.log(res.body.message);
-        fs.writeFile('img.txt', res.body.message, (err) => {
-          console.log('saved');
-        });
-      })
-      .catch((err) => {
-        console.log('err');
-        console.log(err.message);
-      });
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); // this promise tipe that can return resolve and reject
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('imgs.txt', res.body.message); // this promise get from promise before and return resolve and reject
+  })
+  .then(() => {
+    console.log('saved');
   })
   .catch((err) => {
     console.log(err);
   });
 
+// t
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
 // });
