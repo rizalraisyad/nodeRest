1d2fb5dbb35a347ee3876269e5b6406a5af7916f
rizalraisyad <muharamrizal2@gmail.com>
2021-04-03 23:55:45 +0700

    simply module exports calc

--- moduleCalculator/mainCalc.js
+++ moduleCalculator/mainCalc.js
@@ -1,3 +1,9 @@
+// module.export;
+
 const Calculator = require('./calc');
 const mainCalc = new Calculator();
 console.log(mainCalc.add(2, 5));
+
+//exports
+const calculator2 = require('./calc2');
+console.log(calculator2.add(2, 5));
