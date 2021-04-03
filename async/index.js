9dd45dfa7135d3a1d409b97d38ea067f361cb777
rizalraisyad <muharamrizal2@gmail.com>
2021-04-04 01:30:08 +0700

    throw err with return string from func

--- async/index.js
+++ async/index.js
@@ -21,22 +21,52 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); // this promise tipe that can return resolve and reject
-  })
-  .then((res) => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro('imgs.txt', res.body.message); // this promise get from promise before and return resolve and reject
-  })
-  .then(() => {
+    await writeFilePro('img.txt', res.body.message);
     console.log('saved');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  return 'READYYYY';
+};
+
+console.log('1: will get dog pics');
+let a;
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('2: Done gettig dog pics');
+    a = x;
   })
   .catch((err) => {
     console.log(err);
   });
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); // this promise tipe that can return resolve and reject
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro('imgs.txt', res.body.message); // this promise get from promise before and return resolve and reject
+//   })
+//   .then(() => {
+//     console.log('saved');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // t
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
