d1ab7d0044532fd86895423b9dec6f2e98f356fc
rizalraisyad <muharamrizal2@gmail.com>
2021-04-04 00:04:45 +0700

    catch module and exe func

--- moduleCalculator/mainCalc.js
+++ moduleCalculator/mainCalc.js
@@ -7,3 +7,11 @@ console.log(mainCalc.add(2, 5));
 //exports
 const calculator2 = require('./calc2');
 console.log(calculator2.add(2, 5));
+
+//exports
+const { add, multiply, divide } = require('./calc2');
+console.log(add(2, 5));
+
+require('./calc3')();
+require('./calc3')();
+require('./calc3')();
